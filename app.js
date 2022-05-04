daa8f229cf303ccac25d3961f95efca7c410a373
1romans1 <1romans1@gmail.com>
2022-05-04 19:20:18 +0300

    Add environment vars again

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] } );
+const secret = "TheLongAndStrongSecretPhrase!";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] } );
 
 const User = new mongoose.model("User", userSchema);
 
